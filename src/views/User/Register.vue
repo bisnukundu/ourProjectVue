e8c7bb754feb354480b5a666af31f92644d3e558
bisnukundu <bisnukundu@gmail.com>
2022-09-30 15:16:06 +0600

    working with level page

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -13,7 +13,7 @@ const router = useRouter();
 const route = useRoute();
 let error = ref("");
 
-// just for creating some dummy data
+// just for creating some dummy data --Bisnu kundu
 let date = new Date();
 const RegisterData = reactive({
   full_name: `Bisnu${data.getSeconds()} kundu`,
