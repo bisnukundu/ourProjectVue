1263e32c849a932c61adbce840e032cb81df521d
Bisnu kundu <bisnukundu@gmail.com>
2022-10-25 15:39:38 +0600

    modify dashaboard page

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -6,15 +6,8 @@ import Layout from "../../components/Dashboard/Layout.vue";
   <layout>
     <div class="grid grid-cols-2 gap-10 mt-10">
       <div class="bg-slate-800 p-10 rounded-md">
-        <h1 class="font-bold italic text-lg">আপনার বর্তমান ব্যালেন্স!</h1>
-        <p class="mt-2">
-          <b>৮০০ টাকা</b>
-        </p>
-      </div>
-
-      <div class="bg-slate-800 p-10 rounded-md">
-        <h1 class="font-bold italic">আপনার লেভেল!</h1>
-        <p></p>
+        <h1 class="font-bold italic text-lg">স্বাগতম</h1>
+        <p class="mt-2"></p>
       </div>
     </div>
   </layout>
